02559065e232b7eebbbed1e0ae0f1fec04198c84
Saoudatou Diarra <saoudatou.diarra@odc.local>
2021-11-19 15:31:48 +0000

    activite

--- backend/src/main/java/com/example/backend/repository/ActiviteRepository.java
+++ backend/src/main/java/com/example/backend/repository/ActiviteRepository.java
@@ -1,4 +1,7 @@
 package com.example.backend.repository;
 
-public class ActiviteRepository {
+import com.example.backend.model.Activite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActiviteRepository extends JpaRepository<Activite, Long> {
 }
