769c7e5d352fcdc510315f7bf9bac07919afa63b
MamoutouBagayoko <bagayoko1mamoutou@gmail.com>
2021-11-19 14:50:46 +0000

    mise en jour

--- backend/src/main/java/com/example/backend/model/Participant.java
+++ backend/src/main/java/com/example/backend/model/Participant.java
@@ -1,8 +1,18 @@
 package com.example.backend.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import java.io.Serializable;
-
+@Data
 @Entity
 public class Participant implements Serializable {
+    private long id_participant ;
+    private String nom_complet ;
+    private String telephone;
+    private String domaine;
+    private String structure;
+    private String email;
+
+
 }
