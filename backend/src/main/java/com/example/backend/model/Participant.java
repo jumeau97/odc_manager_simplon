50b7497a48efc7c0f23f8914d129e7d1567ea718
MamoutouBagayoko <bagayoko1mamoutou@gmail.com>
2021-11-19 19:18:25 +0000

    backend participant

--- backend/src/main/java/com/example/backend/model/Participant.java
+++ backend/src/main/java/com/example/backend/model/Participant.java
@@ -1,15 +1,24 @@
 package com.example.backend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-@Data
+@Data @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Participant implements Serializable {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private long id_participant ;
     private String nom_complet ;
-    private String telephone;
+    private Integer telephone;
     private String domaine;
     private String structure;
     private String email;
