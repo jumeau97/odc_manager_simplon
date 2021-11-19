80e629a85f14c519ef5edf3ddb732af2218ebf82
Aboubacar Koné <aboubacar.kone@odc.local>
2021-11-19 09:56:02 +0000

    configuration du fichier properties

--- backend/src/main/java/com/example/backend/model/Participant.java
+++ backend/src/main/java/com/example/backend/model/Participant.java
@@ -1,8 +1,7 @@
 package com.example.backend.model;
 
-import javax.persistence.Entity;
-import java.io.Serializable;
 
-@Entity
-public class Participant implements Serializable {
+
+
+public class Participant {
 }
