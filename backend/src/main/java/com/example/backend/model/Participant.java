7a80e2c865655c9ce3aba358ee2725c605d523d5
Aboubacar Koné <aboubacar.kone@odc.local>
2021-11-19 09:56:02 +0000

    configuration du fichier properties

--- backend/src/main/java/com/example/backend/model/Participant.java
+++ backend/src/main/java/com/example/backend/model/Participant.java
@@ -1,18 +1,7 @@
 package com.example.backend.model;
 
-import lombok.Data;
-
-import javax.persistence.Entity;
-import java.io.Serializable;
-@Data
-@Entity
-public class Participant implements Serializable {
-    private long id_participant ;
-    private String nom_complet ;
-    private String telephone;
-    private String domaine;
-    private String structure;
-    private String email;
 
 
+
+public class Participant {
 }
