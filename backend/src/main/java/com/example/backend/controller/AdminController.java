843bd8a5d0cb02368afdd67efefee418f623cc70
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2021-11-19 14:53:13 +0000

    administrateur complète

--- backend/src/main/java/com/example/backend/controller/AdminController.java
+++ backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -1,4 +1,22 @@
 package com.example.backend.controller;
 
+import com.example.backend.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("odcmanager/api")
 public class AdminController {
+
+    @Autowired
+    AdminService adminService;
+    
+@GetMapping("/list")
+    public ResponseEntity<?> list(){
+    return new ResponseEntity<>(adminService.list(), HttpStatus.OK);
+}
+
 }
--- backend/src/main/java/com/example/backend/service/AdminService.java
+++ backend/src/main/java/com/example/backend/service/AdminService.java
@@ -1,4 +1,19 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Administrateur;
+import com.example.backend.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AdminService {
+
+    @Autowired
+    AdminRepository adminRepository;
+
+    public List<Administrateur> list(){
+        return adminRepository.findAll();
+    }
 }
