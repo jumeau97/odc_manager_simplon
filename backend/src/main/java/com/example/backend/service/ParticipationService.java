5c5267274f92b48c5c6d14f68ceafec3a10d8b77
Mohamed Lamine Koné <mohamedlamine.kone@odc.local>
2021-11-20 13:08:40 +0000

    Participation crud

---
+++ backend/src/main/java/com/example/backend/service/ParticipationService.java
@@ -0,0 +1,18 @@
+package com.example.backend.service;
+
+import com.example.backend.model.Participation;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public interface ParticipationService {
+
+    public Participation ajouterParticipation(Participation p, Long id);
+    void deleteParticipation(Long id);
+    public Participation updateParticipation(Long id, Participation p);
+    public List<Participation> getAllParticipation();
+    public Participation getParticipationById(Long id);
+
+}
