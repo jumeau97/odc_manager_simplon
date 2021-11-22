4c9a918e1025bdefeea1cf39e99361a5bbe161e3
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2021-11-22 11:58:53 +0000

    import ko

--- backend/src/main/java/com/example/backend/repository/AdminRepository.java
+++ backend/src/main/java/com/example/backend/repository/AdminRepository.java
@@ -1,14 +1,10 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Administrateur;
-import com.example.backend.model.Etat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import javax.transaction.Transactional;
+
 
 @RepositoryRestResource
 public interface AdminRepository extends JpaRepository<Administrateur, Long> {
