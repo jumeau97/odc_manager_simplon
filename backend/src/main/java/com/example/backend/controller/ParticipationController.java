eaf1260a7777a09bd14f7052b1ef831487be01ce
Mohamed Lamine Koné <mohamedlamine.kone@odc.local>
2021-11-22 15:58:53 +0000

    Participation crud

--- backend/src/main/java/com/example/backend/controller/ParticipationController.java
+++ backend/src/main/java/com/example/backend/controller/ParticipationController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api")
+@RequestMapping("/api/participation")
 public class ParticipationController {
 
     @Autowired
