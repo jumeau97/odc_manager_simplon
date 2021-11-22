87adce1da512eedfd27ee5efa0391cffbf476ca8
Ibrahim Kelly <57602469+hello99world99@users.noreply.github.com>
2021-11-22 12:20:02 +0000

    Updating...

--- backend/src/main/java/com/example/backend/repository/AdminRepository.java
+++ backend/src/main/java/com/example/backend/repository/AdminRepository.java
@@ -8,8 +8,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AdminRepository extends JpaRepository<Administrateur, Long> {
-
-
-
-
 }
