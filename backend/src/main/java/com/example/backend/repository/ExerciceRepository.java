06218cfe507ae4096efdde64b5870491e5dec0c7
daouSam <sambadaou61@gmail.com>
2021-11-22 14:10:38 +0000

    properties

--- backend/src/main/java/com/example/backend/repository/ExerciceRepository.java
+++ backend/src/main/java/com/example/backend/repository/ExerciceRepository.java
@@ -1,1 +1,11 @@
+package com.example.backend.repository;
 
+import com.example.backend.model.Exercice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExerciceRepository extends JpaRepository<Exercice, Long> {
+    public List<Exercice> getExerciceByAnnee(String annee);
+
+}
